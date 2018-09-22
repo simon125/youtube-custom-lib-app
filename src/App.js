4cdd0ce559230435ceb50a2ae207f2881637e1b9
Szymon Oleszek <szymqwerty@gmail.com>
2018-09-23 00:52:18 +0200

    added working input:text with fetch data, without validation

--- src/App.js
+++ src/App.js
@@ -10,13 +10,29 @@ class App extends Component {
 
 
   state = {
-    visible: true,
+    visible: false,
+    results: null,
 
   }
 
   componentDidMount() {
 
   }
+
+  getVideoInfo(urlOrId) {
+    fetch(`https://www.googleapis.com/youtube/v3/videos?key=AIzaSyBfJ3rGTb5Xm56_02-DSSLzYesJb5lKaoY&id=${urlOrId}&part=snippet,statistics`)
+      .then(response => response.json())
+      .then(results => console.log(results))
+  }
+
+
+  handleOnSubmitAddForm = e => {
+    e.preventDefault()
+    const phrase = e.target.urlOrId.value
+    this.getVideoInfo(phrase)
+  }
+
+
 
   handleClick = () => {
     this.setState({ visible: !this.state.visible })
@@ -26,11 +42,12 @@ class App extends Component {
 
   render() {
 
-    // this.getSearchResult('phrase')
 
     return (
       <div>
-
+        <AddForm
+          handleOnSubmitAddForm={this.handleOnSubmitAddForm}
+        />
         <button onClick={this.handleClick} className="btn btn-primary">Toggle</button>
 
         <MyModal handleClick={this.handleClick} visible={this.state.visible} />
--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
 
 class AddForm extends Component {
+
+
+
+
+
     render() {
+
+        const { handleOnSubmitAddForm } = this.props
+
+
         return (
-            <div className="jumbotron bg-secondary">
-                <form>
+            <div className="jumbotron bg-secondary text-light">
+                <form onSubmit={handleOnSubmitAddForm}>
                     <div className="form-group">
-                        <label htmlFor="pastedUrl">URL or ID: </label>
-                        <input type="text" id="pastedUrl" className="form-control" />
+                        <label htmlFor="urlOrId">URL or ID: </label>
+                        <input type="text" placeholder="Enter url or id of film..." id="urlOrId" className="form-control" />
+                    </div>
+                    <div className="form-group">
+
+                        <input type="submit" value="SUBMIT" className="btn btn-success btn-lg btn-block" />
                     </div>
                 </form>
             </div>
